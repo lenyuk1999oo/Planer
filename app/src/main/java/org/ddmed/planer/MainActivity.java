40e2212fb6294f21709d4de681973e6a55e448b6
lenyuk1999oo <lenyuk1999oo@gmail.com>
2020-12-23 13:58:51 +0200

    yes

--- app/src/main/java/org/ddmed/planer/MainActivity.java
+++ app/src/main/java/org/ddmed/planer/MainActivity.java
@@ -2,29 +2,188 @@ package org.ddmed.planer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.InputType;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.hl7.fhir.r4.model.Location;
 import org.hl7.fhir.r4.model.Patient;
+
+import java.util.Calendar;
+import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import com.framgia.library.calendardayview.CalendarDayView;
+import com.framgia.library.calendardayview.EventView;
+import com.framgia.library.calendardayview.PopupView;
+import com.framgia.library.calendardayview.data.IEvent;
+import com.framgia.library.calendardayview.data.IPopup;
+import com.framgia.library.calendardayview.decoration.CdvDecorationDefault;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import ca.uhn.fhir.rest.client.exceptions.FhirClientConnectionException;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    private TextView textView;
+    private Spinner spinner;
+    private Button btnOk;
+    private Button btnSelect;
+    private EditText eText;
+    private DatePickerDialog picker;
+
+    CalendarDayView dayView;
+
+    ArrayList<IEvent> events;
+    ArrayList<IPopup> popups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textView);
+        //textView = (TextView) findViewById(R.id.textView);
+        spinner = (Spinner) findViewById(R.id.spinner1);
+        eText = (EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        final Calendar cldr = Calendar.getInstance();
+        int day = cldr.get(Calendar.DAY_OF_MONTH);
+        int month = cldr.get(Calendar.MONTH);
+        int year = cldr.get(Calendar.YEAR);
+        eText.setText(day + "/" + (month + 1) + "/" + year);
 
-        View.OnClickListener onClickListener = new View.OnClickListener() {
+        String[] items = new String[]{"1", "2", "three"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinner.setAdapter(adapter);
+
+        addOnClickListener();
+        calenderTest();
+    }
+
+    public void calenderTest() {
+        dayView = (CalendarDayView) findViewById(R.id.dayView);
+        dayView.setLimitTime(9, 22);
+
+        ((CdvDecorationDefault) (dayView.getDecoration())).setOnEventClickListener(
+                new EventView.OnEventClickListener() {
+                    @Override
+                    public void onEventClick(EventView view, IEvent data) {
+                        Log.e("TAG", "onEventClick:" + data.getName());
+                    }
+
+                    @Override
+                    public void onEventViewClick(View view, EventView eventView, IEvent data) {
+                        Log.e("TAG", "onEventViewClick:" + data.getName());
+                        if (data instanceof Event) {
+                            // change event (ex: set event color)
+                            dayView.setEvents(events);
+                        }
+                    }
+                });
+
+        ((CdvDecorationDefault) (dayView.getDecoration())).setOnPopupClickListener(
+                new PopupView.OnEventPopupClickListener() {
+                    @Override
+                    public void onPopupClick(PopupView view, IPopup data) {
+                        Log.e("TAG", "onPopupClick:" + data.getTitle());
+                    }
+
+                    @Override
+                    public void onQuoteClick(PopupView view, IPopup data) {
+                        Log.e("TAG", "onQuoteClick:" + data.getTitle());
+                    }
+
+                    @Override
+                    public void onLoadData(PopupView view, ImageView start, ImageView end,
+                                           IPopup data) {
+                        start.setImageResource(R.drawable.logo_angelholm);
+                    }
+                });
+
+        events = new ArrayList<>();
+
+        {
+            int eventColor = ContextCompat.getColor(this, R.color.eventColor);
+            Calendar timeStart = Calendar.getInstance();
+            timeStart.set(Calendar.HOUR_OF_DAY, 11);
+            timeStart.set(Calendar.MINUTE, 0);
+            Calendar timeEnd = (Calendar) timeStart.clone();
+            timeEnd.set(Calendar.HOUR_OF_DAY, 15);
+            timeEnd.set(Calendar.MINUTE, 30);
+            Event event = new Event(1, timeStart, timeEnd, "Event", "Hockaido", eventColor);
+
+            events.add(event);
+        }
+
+        {
+            int eventColor = ContextCompat.getColor(this, R.color.eventColor1);
+            Calendar timeStart = Calendar.getInstance();
+            timeStart.set(Calendar.HOUR_OF_DAY, 18);
+            timeStart.set(Calendar.MINUTE, 0);
+            Calendar timeEnd = (Calendar) timeStart.clone();
+            timeEnd.set(Calendar.HOUR_OF_DAY, 20);
+            timeEnd.set(Calendar.MINUTE, 30);
+            Event event = new Event(1, timeStart, timeEnd, "Another event", "Hockaido", eventColor);
+
+            events.add(event);
+        }
+
+        popups = new ArrayList<>();
+
+        {
+            Calendar timeStart = Calendar.getInstance();
+            timeStart.set(Calendar.HOUR_OF_DAY, 12);
+            timeStart.set(Calendar.MINUTE, 0);
+            Calendar timeEnd = (Calendar) timeStart.clone();
+            timeEnd.set(Calendar.HOUR_OF_DAY, 13);
+            timeEnd.set(Calendar.MINUTE, 30);
+
+            Popup popup = new Popup();
+            popup.setStartTime(timeStart);
+            popup.setEndTime(timeEnd);
+            popup.setImageStart("http://sample.com/image.png");
+            popup.setTitle("event 1 with title");
+            popup.setDescription("Yuong alsdf");
+            popups.add(popup);
+        }
+
+        {
+            Calendar timeStart = Calendar.getInstance();
+            timeStart.set(Calendar.HOUR_OF_DAY, 20);
+            timeStart.set(Calendar.MINUTE, 0);
+            Calendar timeEnd = (Calendar) timeStart.clone();
+            timeEnd.set(Calendar.HOUR_OF_DAY, 22);
+            timeEnd.set(Calendar.MINUTE, 0);
+
+            Popup popup = new Popup();
+            popup.setStartTime(timeStart);
+            popup.setEndTime(timeEnd);
+            popup.setImageStart("http://sample.com/image.png");
+            popup.setTitle("event 2 with title");
+            popup.setDescription("Yuong alsdf");
+            popups.add(popup);
+        }
+
+        dayView.setEvents(events);
+        dayView.setPopups(popups);
+    }
+
+    private void addOnClickListener() {
+        btnOk = (Button) findViewById(R.id.button);
+        btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String[] s = {""};
+                final String[] s = {"", ""};
                 Handler handler = new Handler();
                 Thread thread = new Thread(new Runnable() {
 
@@ -35,9 +194,15 @@ public class MainActivity extends AppCompatActivity {
                         for (Patient patient : patientFhirHelper.getPatients())
                             s[0] += patient.getNameFirstRep().getFamily() + " ";
 
+                        LocationFhirHelper locationFhirHelper = new LocationFhirHelper();
+                        for (Location location : locationFhirHelper.getLocation())
+                            s[1] += location.getName() + " ";
+
                         handler.post(new Runnable() {
                             public void run() {
-                                textView.setText(s[0]);
+                                //textView.setText(s[0]);
+                                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, s[1].split(" "));
+                                spinner.setAdapter(adapter);
                             }
                         });
                     }
@@ -45,8 +210,34 @@ public class MainActivity extends AppCompatActivity {
 
                 thread.start();
             }
-        };
-        Button btnOk = (Button) findViewById(R.id.button);
-        btnOk.setOnClickListener(onClickListener);
+        });
+
+        btnSelect = (Button) findViewById(R.id.select);
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //textView.setText("select :" + spinner.getSelectedItem().toString() + " " + eText.getText());
+            }
+        });
+
+        eText = (EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int day = Integer.parseInt(eText.getText().toString().split("/")[0]);
+                int month = Integer.parseInt(eText.getText().toString().split("/")[1]) - 1;
+                int year = Integer.parseInt(eText.getText().toString().split("/")[2]);
+                // date picker dialog
+                picker = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
     }
 }
