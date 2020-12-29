b9d7132d29b1d6eb10140411b1c4f73471b23786
lenyuk1999oo <lenyuk1999oo@gmail.com>
2020-12-29 13:02:44 +0200

    yes

--- app/src/main/java/org/ddmed/planer/MainActivity.java
+++ app/src/main/java/org/ddmed/planer/MainActivity.java
@@ -6,11 +6,15 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -41,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     private Button btnSelect;
     private EditText eText;
     private DatePickerDialog picker;
+    String[] DayOfWeek = {"Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday"};
 
     CalendarDayView dayView;
 
@@ -67,6 +73,60 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         addOnClickListener();
+
+        final Button btnOpenPopup = (Button) findViewById(R.id.button2);
+        btnOpenPopup.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater =
+                        (LayoutInflater)getBaseContext()
+                                .getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = layoutInflater.inflate(R.layout.popup, null);
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+
+                Spinner popupSpinner = (Spinner)popupView.findViewById(R.id.popupspinner);
+
+                ArrayAdapter<String> adapter =
+                        new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_item, DayOfWeek);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                popupSpinner.setAdapter(adapter);
+
+                btnDismiss.setOnClickListener(new Button.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }});
+
+                popupWindow.showAsDropDown(btnOpenPopup, 50, -30);
+
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    int orgX, orgY;
+                    int offsetX, offsetY;
+
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                orgX = (int) event.getX();
+                                orgY = (int) event.getY();
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+                                offsetX = (int)event.getRawX() - orgX;
+                                offsetY = (int)event.getRawY() - orgY;
+                                popupWindow.update(offsetX, offsetY, -1, -1, true);
+                                break;
+                        }
+                        return true;
+                    }});
+            }
+
+        });
         calenderTest();
     }
 
